2180aaf5a3034096274c566d9f710604a4787da4
acehinnnqru <acehinnnqru@gmail.com>
2022-11-21 20:41:36 +0800

    chore: add error handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use crossterm::{
     event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState},
     terminal,
 };
-use std::io::{self, Read};
+use std::{
+    io::{self, Read},
+    time::Duration,
+};
 
 struct CleanUp;
 
@@ -12,21 +15,26 @@ impl Drop for CleanUp {
     }
 }
 
-fn main() {
+fn main() -> crossterm::Result<()> {
     let _clean_up = CleanUp;
     terminal::enable_raw_mode().expect("Could not turn on raw mode.");
     loop {
-        if let Event::Key(event) = event::read().expect("Failed to read line") {
-            match event {
-                KeyEvent {
-                    kind: KeyEventKind::Press,
-                    code: KeyCode::Char('q'),
-                    modifiers: event::KeyModifiers::NONE,
-                    state: KeyEventState::NONE,
-                } => break,
-                _ => {}
-            }
-            println!("{:?}\r", event);
-        };
+        if event::poll(Duration::from_millis(500)).expect("Poll error.") {
+            if let Event::Key(event) = event::read().expect("Failed to read line") {
+                match event {
+                    KeyEvent {
+                        kind: KeyEventKind::Press,
+                        code: KeyCode::Char('q'),
+                        modifiers: event::KeyModifiers::NONE,
+                        state: KeyEventState::NONE,
+                    } => break,
+                    _ => {}
+                }
+                println!("{:?}\r", event);
+            };
+        } else {
+            println!("Not input yet.\r");
+        }
     }
+    Ok(())
 }
